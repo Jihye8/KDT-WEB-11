c18f8ef9666bedb7ad2ec4b1c583452250b7efa9
Jihye8 <brenda829@naver.com>
2024-01-04 13:59:39 +0900

    240104

---
+++ 0104/js1.js
@@ -0,0 +1,47 @@
+// alert("링크방식")
+
+// var
+// var name="홍길동";
+
+// var name ="나비";
+// // 값이 할당되지 않을 때는 undefined가 할당
+// var age
+// // 재선언하여 값을 할당 가능
+// age=20;
+// console.log(age);
+// //값이 바뀌어도 할당 가능 
+// age=10;
+// console.log(age);
+
+// 재선언 가능
+// let name="홍길동";
+// console.log(name);
+// name="성춘향";
+// console.log(name);
+
+// const age = 20;
+// age=30;  
+// 값을 다시 할당 할 수 없다. 
+// 대소문자 구분가능
+// const AGE = 20;
+
+let myname = "홍길동";
+var email="@naver.com";
+let city='서울';
+console.log(myname, email, city);
+console.log("안녕하세요 "+myname,"입니다");
+console.log(`안녕하세요 ${myname}입니다`);
+
+let num=123;
+console.log(num);
+const checked = true;
+const show = false;
+console.log(checked, show);
+
+let noData;
+console.log(noData);
+
+let empty = null;
+console.log(empty)
+
+
